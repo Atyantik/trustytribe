0c31206866bc7f677f8de98b81ea28ca00e939f6
chirag patel <chiragpatel@atyantik.com>
2017-06-05 18:34:25 +0530

    add guzzle

---
+++ src/TrustyTribeManager.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chiragpatel
+ * Date: 5/6/17
+ * Time: 5:49 PM
+ */
+
+namespace Atyantik\Trustytribe;
+
+
+use GuzzleHttp\Client;
+
+class TrustyTribeManager
+{
+    protected $privateKey;
+    protected $publicKey;
+    protected $client;
+    z
+    public function __construct($privateKey, $publicKey)
+    {
+        if ($privateKey === null || $privateKey === '') {
+            throw new \Exception("Please provide PrivateKey For API");
+        }
+        if ($publicKey === null || $publicKey === '') {
+            throw new \Exception("Please provide PublicKey For API");
+        }
+        $this->privateKey = $privateKey;
+        $this->publicKey = $publicKey;
+        $this->client = new Client();
+    }
+}
